47bb22128f19eded06b469f97528f58166275e95
omgaut <omgaut2020@gmail.com>
2024-10-10 22:26:47 -0400

    mobile css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,49 @@
+// import React from 'react';
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar navbar-expand-lg fixed-top custom-navbar">
+//       <div className="container">
+//         <a className="navbar-brand" href="/">
+//           <h3><strong><span className="gautam">Om</span> Gautam</strong></h3>
+//         </a>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-toggle="collapse"
+//           data-target="#navbarNav"
+//           aria-controls="navbarNav"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+//           <ul className="navbar-nav">
+//             <li className="nav-item">
+//               <a className="nav-link" href="#projects">
+//                 Projects
+//               </a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="#experience">
+//                 Experience
+//               </a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="#skills">
+//                 Skills
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
 import React from 'react';
 
 const Navbar = () => {
@@ -10,8 +56,8 @@ const Navbar = () => {
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
